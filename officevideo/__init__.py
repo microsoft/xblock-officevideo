95cb2624d6eeb2b69d18cad91782a4695e91e2f0
vinhub <vin@introp.net>
2016-07-01 10:59:13 +0530

    license and copyright

--- officevideo/__init__.py
+++ officevideo/__init__.py
@@ -1,1 +1,4 @@
+""" Copyright (c) Microsoft Corporation. All Rights Reserved. """
+""" Licensed under the MIT license. See LICENSE file on the project webpage for details. """
+
 from .officevideo import OfficeVideoXBlock
--- officevideo/static/js/src/officevideo_edit.js
+++ officevideo/static/js/src/officevideo_edit.js
@@ -1,3 +1,6 @@
+/* Copyright (c) Microsoft Corporation. All Rights Reserved.
+   Licensed under the MIT license. See LICENSE file on the project webpage for details. */
+
 /* Javascript for OfficeVideoXBlock. */
 function OfficeVideoXBlock(runtime, element) {
 
